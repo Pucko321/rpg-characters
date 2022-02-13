59cbe41f448eaea1ec4a7fc76809ddd8cfa0278c
JonasBM <jonas.mela@gmail.com>
2022-02-13 13:55:52 +0100

    FEAT: getters and setters

--- src/com/company/equipment/Item.java
+++ src/com/company/equipment/Item.java
@@ -6,4 +6,30 @@ public class Item {
     private String name;
     private int level;
     private Slot slot;
+
+    // Getters
+    public String getName() {
+        return this.name;
+    }
+
+    public int getLevel() {
+        return this.level;
+    }
+
+    public Slot getSlot() {
+        return this.slot;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public void setSlot(Slot slot) {
+        this.slot = slot;
+    }
 }
