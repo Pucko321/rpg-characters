361a66350b6dfc3afef48ce70e423609b85abdd7
JonasBM <jonas.mela@gmail.com>
2022-02-13 14:43:06 +0100

    FEAT: add fields

--- src/com/company/equipment/Weapon.java
+++ src/com/company/equipment/Weapon.java
@@ -1,4 +1,17 @@
 package com.company.equipment;
 
 public class Weapon {
+    private enum WeaponType {
+        AXE,
+        BOW,
+        DAGGER,
+        HAMMER,
+        STAFF,
+        SWORD,
+        WAND
+    }
+
+    private int damage;
+    private double attackSpeed;
+    private WeaponType weaponType;
 }
