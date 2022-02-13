08a0666eee96bbe394c4cccec513fece20d2059a
JonasBM <jonas.mela@gmail.com>
2022-02-13 15:48:15 +0100

    FEAT: add fields

--- src/com/company/equipment/Armour.java
+++ src/com/company/equipment/Armour.java
@@ -1,4 +1,15 @@
 package com.company.equipment;
 
+import com.company.PrimaryAttribute;
+
 public class Armour {
+    private enum ArmourType {
+        CLOTH,
+        LEATHER,
+        MAIL,
+        PLATE
+    }
+
+    private PrimaryAttribute attributes;
+    private ArmourType armourType;
 }
