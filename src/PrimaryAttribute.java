1a3cbb2b10885b22df4998865c072ad2687bfa1b
JonasBM <jonas.mela@gmail.com>
2022-02-13 12:20:29 +0100

    FEAT: add getters and setters

--- src/PrimaryAttribute.java
+++ src/PrimaryAttribute.java
@@ -4,4 +4,29 @@ public class PrimaryAttribute {
     private int intelligence;
 
 
+    // Getters
+    public int getStrength() {
+        return this.strength;
+    }
+
+    public int getDexterity() {
+        return this.dexterity;
+    }
+
+    public int getIntelligence() {
+        return this.intelligence;
+    }
+
+    // Setters
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public void setDexterity(int dexterity) {
+        this.dexterity = dexterity;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
 }
