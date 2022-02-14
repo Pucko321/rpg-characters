4ce63c252454313c4f0d432a4a9c05adfdadec7c
JonasBM <jonas.mela@gmail.com>
2022-02-14 08:44:32 +0100

    FEAT: create fields

--- src/com/company/character/Character.java
+++ src/com/company/character/Character.java
@@ -1,4 +1,15 @@
 package com.company.character;
 
+import com.company.Enum.Slot;
+import com.company.PrimaryAttribute;
+import com.company.equipment.Item;
+
+import java.util.HashMap;
+
 public class Character {
+    private String name;
+    private int level;
+    private PrimaryAttribute attributes;
+    private String primaryAttribute;
+    private HashMap<Slot, Item> equipment;
 }
