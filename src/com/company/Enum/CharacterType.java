73517afe94d9c27e3ddc72c94f27cacc9f12cbe8
JonasBM <jonas.mela@gmail.com>
2022-02-15 16:02:12 +0100

    FEAT: add constants related to character type

--- src/com/company/Enum/CharacterType.java
+++ src/com/company/Enum/CharacterType.java
@@ -1,4 +1,26 @@
 package com.company.Enum;
 
 public enum CharacterType {
+    MAGE(   1, 1, 8, 1, 1, 5, "Intelligence"),
+    RANGER( 1, 7, 1, 1, 5, 1, "Dexterity"),
+    ROGUE(  2, 6, 1, 1, 1, 4, "Dexterity"),
+    WARRIOR(5, 2, 1, 3, 2, 1, "Strength");
+
+    final public int defaultStrength;
+    final public int defaultDexterity;
+    final public int defaultIntelligence;
+    final public int strengthGain;
+    final public int dexterityGain;
+    final public int intelligenceGain;
+    final public String primaryAttribute;
+
+    CharacterType(int defaultStrength, int defaultDexterity, int defaultIntelligence, int strengthGain, int dexterityGain, int intelligenceGain, String primaryAttribute) {
+        this.defaultStrength = defaultStrength;
+        this.defaultDexterity = defaultDexterity;
+        this.defaultIntelligence = defaultIntelligence;
+        this.strengthGain = strengthGain;
+        this.dexterityGain = dexterityGain;
+        this.intelligenceGain = intelligenceGain;
+        this.primaryAttribute = primaryAttribute;
+    }
 }
