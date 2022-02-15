bee8f0f8e39a90cb84a190cce7cc94ae6314ba71
JonasBM <jonas.mela@gmail.com>
2022-02-15 16:17:40 +0100

    REFACTOR: change attribute type

--- src/com/company/Enum/CharacterType.java
+++ src/com/company/Enum/CharacterType.java
@@ -1,10 +1,10 @@
 package com.company.Enum;
 
 public enum CharacterType {
-    MAGE(   1, 1, 8, 1, 1, 5, "Intelligence"),
-    RANGER( 1, 7, 1, 1, 5, 1, "Dexterity"),
-    ROGUE(  2, 6, 1, 1, 1, 4, "Dexterity"),
-    WARRIOR(5, 2, 1, 3, 2, 1, "Strength");
+    MAGE(   1, 1, 8, 1, 1, 5, PrimaryAttributeType.INTELLIGENCE),
+    RANGER( 1, 7, 1, 1, 5, 1, PrimaryAttributeType.DEXTERITY),
+    ROGUE(  2, 6, 1, 1, 1, 4, PrimaryAttributeType.DEXTERITY),
+    WARRIOR(5, 2, 1, 3, 2, 1, PrimaryAttributeType.STRENGTH);
 
     final public int defaultStrength;
     final public int defaultDexterity;
@@ -12,9 +12,9 @@ public enum CharacterType {
     final public int strengthGain;
     final public int dexterityGain;
     final public int intelligenceGain;
-    final public String primaryAttribute;
+    final public PrimaryAttributeType primaryAttribute;
 
-    CharacterType(int defaultStrength, int defaultDexterity, int defaultIntelligence, int strengthGain, int dexterityGain, int intelligenceGain, String primaryAttribute) {
+    CharacterType(int defaultStrength, int defaultDexterity, int defaultIntelligence, int strengthGain, int dexterityGain, int intelligenceGain, PrimaryAttributeType primaryAttribute) {
         this.defaultStrength = defaultStrength;
         this.defaultDexterity = defaultDexterity;
         this.defaultIntelligence = defaultIntelligence;
