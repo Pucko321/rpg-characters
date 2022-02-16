d67b3f0e349e630ce5b48cfc655e6597b263778e
JonasBM <jonas.mela@gmail.com>
2022-02-16 15:56:09 +0100

    CHORE(Weapon): create test class

---
+++ test/com/company/equipment/WeaponTest.java
@@ -0,0 +1,30 @@
+package com.company.equipment;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WeaponTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void getDPS() {
+    }
+
+    @Test
+    void setDamage() {
+    }
+
+    @Test
+    void setAttackSpeed() {
+    }
+
+    @Test
+    void setWeaponType() {
+    }
+}
