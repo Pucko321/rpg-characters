22b832acc15e412c68d80a1c48cce29f01dae7ee
JonasBM <jonas.mela@gmail.com>
2022-02-16 19:40:22 +0100

    TEST(RogueTest): add tests

--- test/com/company/character/RogueTest.java
+++ test/com/company/character/RogueTest.java
@@ -1,6 +1,8 @@
 package com.company.character;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,5 +13,56 @@ class RogueTest {
     @BeforeEach
     void setUp() {
         testRogue = new Rogue(originalName);
+    }
+
+    @Test
+    void correctLevelWhenCreated_ValidValue_ShouldPass() {
+        int expected = 1;
+        int result = testRogue.getLevel();
+
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    void correctLevelWhenLevelUp_ValidValue_ShouldPass() {
+        int expected = 2;
+
+        testRogue.levelUp();
+
+        int result = testRogue.getLevel();
+
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    void correctAttributesWhenCreated_ValidValue_ShouldPass() {
+        int expectedStrength = 2;
+        int expectedDexterity = 6;
+        int expectedIntelligence = 1;
+
+        int strengthResult = testRogue.getStrength();
+        int dexterityResult = testRogue.getDexterity();
+        int intelligenceResult = testRogue.getIntelligence();
+
+        Assertions.assertEquals(expectedStrength, strengthResult);
+        Assertions.assertEquals(expectedDexterity, dexterityResult);
+        Assertions.assertEquals(expectedIntelligence, intelligenceResult);
+    }
+
+    @Test
+    void correctAttributesWhenLevelUp_ValidValue_ShouldPass() {
+        int expectedStrength = 3;
+        int expectedDexterity = 10;
+        int expectedIntelligence = 2;
+
+        testRogue.levelUp();
+
+        int strengthResult = testRogue.getStrength();
+        int dexterityResult = testRogue.getDexterity();
+        int intelligenceResult = testRogue.getIntelligence();
+
+        Assertions.assertEquals(expectedStrength, strengthResult);
+        Assertions.assertEquals(expectedDexterity, dexterityResult);
+        Assertions.assertEquals(expectedIntelligence, intelligenceResult);
     }
 }
