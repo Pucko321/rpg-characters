23f308e0128bef8a0559bc8e187e997d52a389a7
JonasBM <jonas.mela@gmail.com>
2022-02-16 18:17:54 +0100

    TEST(MageTest): correct attributes

--- test/com/company/character/MageTest.java
+++ test/com/company/character/MageTest.java
@@ -1,5 +1,6 @@
 package com.company.character;
 
+import com.company.PrimaryAttribute;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -32,5 +33,21 @@ class MageTest {
         int result = testMage.getLevel();
 
         Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    void correctAttributesWhenCreated_ValidValue_ShouldPass() {
+        int expectedStrength = 1;
+        int expectedDexterity = 1;
+        int expectedIntelligence = 8;
+
+        int strengthResult = testMage.getStrength();
+        int dexterityResult = testMage.getDexterity();
+        int intelligenceResult = testMage.getIntelligence();
+
+
+        Assertions.assertEquals(expectedStrength, strengthResult);
+        Assertions.assertEquals(expectedDexterity, dexterityResult);
+        Assertions.assertEquals(expectedIntelligence, intelligenceResult);
     }
 }
