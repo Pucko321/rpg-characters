d6a4b80019994c55e18630592c7bffe9f0d863e5
JonasBM <jonas.mela@gmail.com>
2022-02-16 19:35:26 +0100

    CHORE(RogueTest): add setup

--- test/com/company/character/RogueTest.java
+++ test/com/company/character/RogueTest.java
@@ -5,8 +5,11 @@ import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RogueTest {
+    Rogue testRogue;
+    String originalName = "Test name";
 
     @BeforeEach
     void setUp() {
+        testRogue = new Rogue(originalName);
     }
 }
