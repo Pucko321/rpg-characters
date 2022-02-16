62e4cb45b6c4dfe9d5c66ee9c82ce659a781d23c
JonasBM <jonas.mela@gmail.com>
2022-02-16 19:23:35 +0100

    CHORE: create test class for ranger

---
+++ test/com/company/character/RangerTest.java
@@ -0,0 +1,12 @@
+package com.company.character;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RangerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
