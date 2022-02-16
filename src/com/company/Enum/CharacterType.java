910cfed9fedabf6ec31b08dc406daa35c5ad3838
JonasBM <jonas.mela@gmail.com>
2022-02-16 23:06:13 +0100

    FEAT: add supported weapon types

--- src/com/company/Enum/CharacterType.java
+++ src/com/company/Enum/CharacterType.java
@@ -1,10 +1,10 @@
 package com.company.Enum;
 
 public enum CharacterType {
-    MAGE(   1, 1, 8, 1, 1, 5, PrimaryAttributeType.INTELLIGENCE),
-    RANGER( 1, 7, 1, 1, 5, 1, PrimaryAttributeType.DEXTERITY),
-    ROGUE(  2, 6, 1, 1, 4, 1, PrimaryAttributeType.DEXTERITY),
-    WARRIOR(5, 2, 1, 3, 2, 1, PrimaryAttributeType.STRENGTH);
+    MAGE(   1, 1, 8, 1, 1, 5, PrimaryAttributeType.INTELLIGENCE, new WeaponType[]{WeaponType.STAFF, WeaponType.WAND}),
+    RANGER( 1, 7, 1, 1, 5, 1, PrimaryAttributeType.DEXTERITY, new WeaponType[]{WeaponType.BOW}),
+    ROGUE(  2, 6, 1, 1, 4, 1, PrimaryAttributeType.DEXTERITY, new WeaponType[]{WeaponType.DAGGER, WeaponType.SWORD}),
+    WARRIOR(5, 2, 1, 3, 2, 1, PrimaryAttributeType.STRENGTH, new WeaponType[]{WeaponType.AXE, WeaponType.HAMMER, WeaponType.SWORD});
 
     final public int defaultStrength;
     final public int defaultDexterity;
@@ -13,8 +13,9 @@ public enum CharacterType {
     final public int dexterityGain;
     final public int intelligenceGain;
     final public PrimaryAttributeType primaryAttribute;
+    final public WeaponType[] supportedWeaponTypes;
 
-    CharacterType(int defaultStrength, int defaultDexterity, int defaultIntelligence, int strengthGain, int dexterityGain, int intelligenceGain, PrimaryAttributeType primaryAttribute) {
+    CharacterType(int defaultStrength, int defaultDexterity, int defaultIntelligence, int strengthGain, int dexterityGain, int intelligenceGain, PrimaryAttributeType primaryAttribute, WeaponType[] supportedWeaponTypes) {
         this.defaultStrength = defaultStrength;
         this.defaultDexterity = defaultDexterity;
         this.defaultIntelligence = defaultIntelligence;
@@ -22,5 +23,6 @@ public enum CharacterType {
         this.dexterityGain = dexterityGain;
         this.intelligenceGain = intelligenceGain;
         this.primaryAttribute = primaryAttribute;
+        this.supportedWeaponTypes = supportedWeaponTypes;
     }
 }
