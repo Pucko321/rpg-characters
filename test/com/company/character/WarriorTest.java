d0040a643d4c638d8350d5c69251b80bdfe42682
JonasBM <jonas.mela@gmail.com>
2022-02-16 19:41:03 +0100

    CHORE(WarriorTest): create test class

---
+++ test/com/company/character/WarriorTest.java
@@ -0,0 +1,12 @@
+package com.company.character;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WarriorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
