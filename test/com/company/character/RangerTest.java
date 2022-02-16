25e04eda4fb80224e53cd5ca115bb10de20f3083
JonasBM <jonas.mela@gmail.com>
2022-02-16 19:25:11 +0100

    CHORE(RangerTest): add setup

--- test/com/company/character/RangerTest.java
+++ test/com/company/character/RangerTest.java
@@ -5,8 +5,11 @@ import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RangerTest {
+    Ranger testMage;
+    String originalName = "Test name";
 
     @BeforeEach
     void setUp() {
+        testMage = new Ranger(originalName);
     }
 }
