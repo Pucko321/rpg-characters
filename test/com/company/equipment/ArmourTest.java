863e83b6d722192cd71a7bbf8ea162f9cf192550
JonasBM <jonas.mela@gmail.com>
2022-02-16 17:20:08 +0100

    CHORE(ArmourTest): create class

---
+++ test/com/company/equipment/ArmourTest.java
@@ -0,0 +1,21 @@
+package com.company.equipment;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArmourTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAttributes() {
+    }
+
+    @Test
+    void getArmourType() {
+    }
+}
