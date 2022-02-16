d559e5aa24a4ecf579eca2e4201b2d27f9146c36
JonasBM <jonas.mela@gmail.com>
2022-02-16 20:13:35 +0100

    FIX: variable scope

--- test/com/company/character/CharacterWithEquipmentTest.java
+++ test/com/company/character/CharacterWithEquipmentTest.java
@@ -4,13 +4,20 @@ import com.company.Enum.Slot;
 import com.company.PrimaryAttribute;
 import com.company.equipment.Armour;
 import com.company.equipment.Weapon;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class CharacterWithEquipmentTest {
+    Weapon testWeapon;
+    Armour testPlateBody;
+    Weapon testBow;
+    Armour testClothHead;
+    Warrior testWarrior;
 
     @BeforeEach
     void setUp() {
-        Weapon testWeapon = new Weapon();
+        testWeapon = new Weapon();
         testWeapon.setName("Common Axe");
         testWeapon.setLevel(1);
         testWeapon.setSlot(Slot.WEAPON);
@@ -18,14 +25,14 @@ class CharacterWithEquipmentTest {
         testWeapon.setDamage(7);
         testWeapon.setAttackSpeed(1.1);
 
-        Armour testPlateBody = new Armour();
+        testPlateBody = new Armour();
         testPlateBody.setName("Common Plate Body Armour");
         testPlateBody.setLevel(1);
         testPlateBody.setSlot(Slot.BODY);
         testPlateBody.setArmourType(Armour.ArmourType.PLATE);
         testPlateBody.setAttributes(new PrimaryAttribute(1, 0, 0));
 
-        Weapon testBow = new Weapon();
+        testBow = new Weapon();
         testBow.setName("Common Bow");
         testBow.setLevel(1);
         testBow.setSlot(Slot.WEAPON);
@@ -33,13 +40,19 @@ class CharacterWithEquipmentTest {
         testBow.setDamage(12);
         testBow.setAttackSpeed(0.8);
 
-        Armour testClothHead = new Armour();
+        testClothHead = new Armour();
         testClothHead.setName("Common Cloth Head Armour");
         testClothHead.setLevel(1);
         testClothHead.setSlot(Slot.HEAD);
         testClothHead.setArmourType(Armour.ArmourType.CLOTH);
         testClothHead.setAttributes(new PrimaryAttribute(0, 0, 5));
+
+        testWarrior = new Warrior();
     }
 
-
+    /*@Test
+    void toHighWeaponLevel_ValidValue_ShouldThrowError() {
+        testWeapon.setLevel(2);
+        Assertions.assertEquals(1, 1);
+    }*/
 }
