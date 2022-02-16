1f65664d2ef73a1a5615c16ceeeba1a046f69960
JonasBM <jonas.mela@gmail.com>
2022-02-16 19:32:07 +0100

    TEST: add testes for ranger

--- test/com/company/character/RangerTest.java
+++ test/com/company/character/RangerTest.java
@@ -1,15 +1,68 @@
 package com.company.character;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RangerTest {
-    Ranger testMage;
+    Ranger testRanger;
     String originalName = "Test name";
 
     @BeforeEach
     void setUp() {
-        testMage = new Ranger(originalName);
+        testRanger = new Ranger(originalName);
+    }
+
+    @Test
+    void correctLevelWhenCreated_ValidValue_ShouldPass() {
+        int expected = 1;
+        int result = testRanger.getLevel();
+
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    void correctLevelWhenLevelUp_ValidValue_ShouldPass() {
+        int expected = 2;
+
+        testRanger.levelUp();
+
+        int result = testRanger.getLevel();
+
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    void correctAttributesWhenCreated_ValidValue_ShouldPass() {
+        int expectedStrength = 1;
+        int expectedDexterity = 7;
+        int expectedIntelligence = 1;
+
+        int strengthResult = testRanger.getStrength();
+        int dexterityResult = testRanger.getDexterity();
+        int intelligenceResult = testRanger.getIntelligence();
+
+        Assertions.assertEquals(expectedStrength, strengthResult);
+        Assertions.assertEquals(expectedDexterity, dexterityResult);
+        Assertions.assertEquals(expectedIntelligence, intelligenceResult);
+    }
+
+    @Test
+    void correctAttributesWhenLevelUp_ValidValue_ShouldPass() {
+        int expectedStrength = 2;
+        int expectedDexterity = 12;
+        int expectedIntelligence = 2;
+
+        testRanger.levelUp();
+
+        int strengthResult = testRanger.getStrength();
+        int dexterityResult = testRanger.getDexterity();
+        int intelligenceResult = testRanger.getIntelligence();
+
+        Assertions.assertEquals(expectedStrength, strengthResult);
+        Assertions.assertEquals(expectedDexterity, dexterityResult);
+        Assertions.assertEquals(expectedIntelligence, intelligenceResult);
     }
 }
