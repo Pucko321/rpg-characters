f836baa954a118b191ac8cd91f0a57ec8eb4924e
JonasBM <jonas.mela@gmail.com>
2022-02-16 19:42:15 +0100

    CHORE(WarriorTest): add setup

--- test/com/company/character/WarriorTest.java
+++ test/com/company/character/WarriorTest.java
@@ -5,8 +5,11 @@ import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
 
 class WarriorTest {
+    Warrior testWarrior;
+    String originalName = "Test name";
 
     @BeforeEach
     void setUp() {
+        testWarrior = new Warrior(originalName);
     }
 }
