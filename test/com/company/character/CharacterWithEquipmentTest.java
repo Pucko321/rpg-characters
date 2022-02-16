6c6da32c4f2764334ae4010925fc0cfb5ff00681
JonasBM <jonas.mela@gmail.com>
2022-02-16 20:04:24 +0100

    CHORE(CharacterWithEquipmentTest): add setup

--- test/com/company/character/CharacterWithEquipmentTest.java
+++ test/com/company/character/CharacterWithEquipmentTest.java
@@ -1,10 +1,45 @@
 package com.company.character;
 
+import com.company.Enum.Slot;
+import com.company.PrimaryAttribute;
+import com.company.equipment.Armour;
+import com.company.equipment.Weapon;
 import org.junit.jupiter.api.BeforeEach;
 
 class CharacterWithEquipmentTest {
 
     @BeforeEach
     void setUp() {
+        Weapon testWeapon = new Weapon();
+        testWeapon.setName("Common Axe");
+        testWeapon.setLevel(1);
+        testWeapon.setSlot(Slot.WEAPON);
+        testWeapon.setWeaponType(Weapon.WeaponType.AXE);
+        testWeapon.setDamage(7);
+        testWeapon.setAttackSpeed(1.1);
+
+        Armour testPlateBody = new Armour();
+        testPlateBody.setName("Common Plate Body Armour");
+        testPlateBody.setLevel(1);
+        testPlateBody.setSlot(Slot.BODY);
+        testPlateBody.setArmourType(Armour.ArmourType.PLATE);
+        testPlateBody.setAttributes(new PrimaryAttribute(1, 0, 0));
+
+        Weapon testBow = new Weapon();
+        testBow.setName("Common Bow");
+        testBow.setLevel(1);
+        testBow.setSlot(Slot.WEAPON);
+        testBow.setWeaponType(Weapon.WeaponType.BOW);
+        testBow.setDamage(12);
+        testBow.setAttackSpeed(0.8);
+
+        Armour testClothHead = new Armour();
+        testClothHead.setName("Common Cloth Head Armour");
+        testClothHead.setLevel(1);
+        testClothHead.setSlot(Slot.HEAD);
+        testClothHead.setArmourType(Armour.ArmourType.CLOTH);
+        testClothHead.setAttributes(new PrimaryAttribute(0, 0, 5));
     }
+
+
 }
